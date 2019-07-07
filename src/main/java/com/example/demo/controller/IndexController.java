4139cd47f00ae7a3af583b5dbc2ffcebd841da71
qieting <2498377527@qq.com>
2019-07-07 11:25:13 +0800

    add index  and bootstrap

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -9,10 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 //Controller的意思为我允许这个类接受前端的请求，使用@Controller，进行并列
 
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
